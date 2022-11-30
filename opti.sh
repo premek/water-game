8ff8aa0113a72ba7bcaf5af5ef2727dd2ce31275
premek <1145361+premek@users.noreply.github.com>
2022-12-01 00:44:58 +0100

    remove absolute path from spritesheet json files

--- opti.sh
+++ opti.sh
@@ -2,8 +2,10 @@ for F in src/img/*png; do
   optipng -strip all "$F"
 done
 
+# remove lines with absolute path to image
+for F in src/img/*json; do sed -i '/"image"/d' "$F"; done
+
 for f in src/*.lua ; do 
     lua src/lib/luafmt.lua --f "$f" 120
     luacheck "$f" --globals love
-
 done
